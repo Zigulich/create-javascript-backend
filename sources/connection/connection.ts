d3ba812601fca4f5e79ea92911e4996858445d6a
Luan Zigulich <39498496+Zigulich@users.noreply.github.com>
2021-05-19 11:14:18 -0300

    fix: corrigido configuração do dotenv para caminhos relativos

--- sources/connection/connection.ts
+++ sources/connection/connection.ts
@@ -1,10 +1,6 @@
 import knex from 'knex'
-import dotent from 'dotenv'
-import path from 'path'
 
-dotent.config({ path: path.resolve(__dirname, './../../.env') })
-
-const { CONNECTION_CLIENT, CONNECTION_HOST, CONNECTION_USER, CONNECTION_PASS, CONNECTION_DB } = process.env;
+const { CONNECTION_CLIENT, CONNECTION_HOST, CONNECTION_USER, CONNECTION_PASS, CONNECTION_DB } = process.env
 
 export default knex({
 	client: CONNECTION_CLIENT,
@@ -13,6 +9,6 @@ export default knex({
 		user: CONNECTION_USER,
 		password: CONNECTION_PASS,
 		database: CONNECTION_DB,
-		filename: CONNECTION_CLIENT === 'sqlite3' ? CONNECTION_DB : ''
-	}
+		filename: CONNECTION_CLIENT === 'sqlite3' ? CONNECTION_DB : '',
+	},
 })
--- sources/server.ts
+++ sources/server.ts
@@ -1,9 +1,10 @@
-import server from './index'
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.resolve(__dirname, './../.env') })
+dotenv.config({ path: path.resolve('./', '.env') })
 
-server.listen(process.env.SERVER_PORT, () => { 
-	console.log(`Servidor aberto na porta ${process.env.SERVER_PORT}`) 
+import server from './index'
+
+server.listen(process.env.SERVER_PORT, () => {
+	console.log(`Servidor aberto na porta ${process.env.SERVER_PORT}`)
 })
