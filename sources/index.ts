8872aabbdbdaa157914a33d80d2211f5d953143a
Luan Zigulich <39498496+Zigulich@users.noreply.github.com>
2021-06-05 13:27:41 -0300

    feat: configurado resposta do tokenCheck com a DefaultResponse

--- sources/index.ts
+++ sources/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
+import DefaultResponse from '@functions/DefaultResponse'
 
 /*{ROUTE_IMPORT}*/
 
 class App {
-	server: express.Application;
+	server: express.Application
 
 	constructor() {
-		this.server = express();
+		this.server = express()
 
 		this.middlewares()
 		this.routes()
@@ -19,22 +20,20 @@ class App {
 	}
 
 	middlewares() {
-		this.server.use(cors());
+		this.server.use(cors())
 		this.server.use(express.json())
 	}
 
-	tokenCheck(req: Request, res: Response, next: NextFunction){
-		const appSecret: string = process.env.JWT_SECRET as string;
-		const requestToken: string = req.headers['x-access-token'] as string;
-		if (!requestToken) return res.status(401).json({ auth: false, message: 'No token provided.' });
-		
-		jwt.verify(requestToken, appSecret, function(err, decoded) {
-			if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-			
-			next();
-		});
-	}
+	tokenCheck(req: Request, res: Response, next: NextFunction) {
+		const appSecret: string = process.env.JWT_SECRET as string
+		const requestToken: string = req.headers['x-access-token'] as string
+		if (!requestToken) return DefaultResponse(false, 'Token não encontrado', {}, res)
 
+		jwt.verify(requestToken, appSecret, function (err, decoded) {
+			if (err) return DefaultResponse(false, 'Falha ao autenticar o token.', err, res)
+			next()
+		})
+	}
 }
 
 export default new App().server
