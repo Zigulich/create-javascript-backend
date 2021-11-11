94e3b52598b41beccd10d8b83cc0852653d27468
Luan Zigulich <ozigulich@gmail.com>
2021-11-11 08:45:35 -0300

    fix: corrigido erro de importação do babel com dotenv

--- sources/server.ts
+++ sources/server.ts
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv'
-import path from 'path'
-
-dotenv.config({ path: path.resolve('./', '.env') })
-
+import './dotenv.config'
 import server from './index'
 
 server.listen(process.env.PORT, () => {
